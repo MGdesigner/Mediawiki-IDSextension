ec57005b5f88ff917a87923a19070e9938322d5d
Adam Roses Wight <awight@wikimedia.org>
2016-06-26 00:53:16 +0200

    Convert spaces to tabs; clean up comment

--- ids_body.php
+++ ids_body.php
@@ -9,16 +9,17 @@ class IDS {
 		$parser->setHook( 'ids', [ __CLASS__, 'idsRender' ] );
 		return true;
 	}
+
 	static function idsRender( $input, array $args, Parser $parser, PPFrame $frame ) {
-	  /*
-	  Example:
-	  $ret ='<img alt="⿰電心" src="https://tools.wmflabs.org/idsgen/⿰電心.png?字體=宋體" width="16" height="16"  />';
-	  */
-	  $ret = '<img  align=middle alt="';
-	  $ret .=$input;
-	  $ret .= '" src="https://tools.wmflabs.org/idsgen/';
-	  $ret .= $input;
-	  $ret .= '.png?字體=宋體" style="height: 1em; width: 1em; vertical-align: middle; margin: 0.4em 0px 0.7em; "  />';
-	  return $ret;
+		/*
+		 * Example output:
+		 * '<img alt="⿰電心" src="https://tools.wmflabs.org/idsgen/⿰電心.png?字體=宋體" width="16" height="16" />'
+		 */
+		$ret = '<img  align=middle alt="';
+		$ret .= $input;
+		$ret .= '" src="https://tools.wmflabs.org/idsgen/';
+		$ret .= $input;
+		$ret .= '.png?字體=宋體" style="height: 1em; width: 1em; vertical-align: middle; margin: 0.4em 0px 0.7em; "  />';
+		return $ret;
 	}
 }
